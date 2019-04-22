fc656aaa3863464d341a2da6775d4e0e60bb691e
bblackfox <43825026+bblackfox@users.noreply.github.com>
2019-04-22 12:37:01 -0500

    change

---
+++ exam3.js
@@ -0,0 +1,17 @@
+const loadJsonFile = require('load-json-file');
+ 
+loadJsonFile('data.json').then(json => {
+    // console.log(json);
+    
+
+
+});
+
+var fs = require('fs');
+
+fs.readFile('data.json', function(err, data){
+    var jsonData = data;
+    var jsonParsed = JSON.parse(jsonData);
+    console.log(jsonParsed.items[0].fname, jsonParsed.items[0].age);
+    console.log(jsonParsed.items[1].fname, jsonParsed.items[1].age);
+})
